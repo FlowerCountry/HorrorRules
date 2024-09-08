849b282b91dcc245095b5e67f03bb853dba06ca2
FlowerCity <admin@flowercity.xyz>
2024-09-08 21:58:44 +0800

    update

--- index.js
+++ index.js
@@ -2,15 +2,21 @@
  * @Author: FlowerCity admin@flowercity.xyz
  * @Date: 2024-09-08 15:14:38
  * @LastEditors: FlowerCity admin@flowercity.xyz
- * @LastEditTime: 2024-09-08 21:43:11
+ * @LastEditTime: 2024-09-08 21:56:43
  * @FilePath: \HorrorRules\index.js
  */
 
 let accumulatedTime = 0;
 
-content.forEach(item => {
+StartContent.forEach(item => {
     accumulatedTime += item.time * 1000;
     setTimeout(() => {
-        console.log(item.content);
+        for (let i = 0; i < document.getElementsByClassName('text').length; i++) {
+            document.getElementsByClassName('text')[i].remove();
+        }
+        let text = document.createElement("div");
+        document.getElementById('start').appendChild(text);
+        text.textContent = item.content;
+        text.classList.add('text');
     }, accumulatedTime);
 });

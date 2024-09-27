15d956707d11ea6fd6e87889b9581d2aed7e1c4f
FlowerCity <admin@flowercity.xyz>
2024-09-27 20:38:18 +0800

    update

--- languages/english.js
+++ languages/english.js
@@ -2,7 +2,7 @@
  * @Author: FlowerCity admin@flowercity.xyz
  * @Date: 2024-09-08 20:55:55
  * @LastEditors: FlowerCity admin@flowercity.xyz
- * @LastEditTime: 2024-09-08 21:43:21
+ * @LastEditTime: 2024-09-15 08:40:42
  * @FilePath: \HorrorRules\languages\english.js
  */
 var StartContent = [
@@ -11,7 +11,7 @@ var StartContent = [
     { content: 'Many students in there.', time: 1.3 },
     { content: 'They can only eat some vagetables.', time: 2 },
     { content: 'But today they was happy.', time: 1.2 },
-    { content: 'Because there is meat in cafeteria.', time: 2.5 },
+    { content: 'Because there is meat in dinner hall.', time: 2.5 },
     { content: 'A student asked,', time: 0.8 },
     { content: '\"Teacher, why there is meat?\"', time: 1.4 },
     { content: 'The teacher answerd,', time: 0.9 },
